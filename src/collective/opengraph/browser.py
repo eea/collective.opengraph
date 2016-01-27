48941a7ea02c52fce58d11000ad0ca63c4464808
Guido A.J. Stevens <guido.stevens@cosent.nl>
2016-01-27 17:54:12 +0100

    fix imports

--- src/collective/opengraph/browser.py
+++ src/collective/opengraph/browser.py
@@ -1,6 +1,6 @@
 from zope.interface import alsoProvides
 from zope.interface import noLongerProvides
-from zope.site.hooks import getSite
+from zope.component.hooks import getSite
 from zope.event import notify
 
 from Products.Five import BrowserView
